2efd648942d8c3e8b05d5cb9ed47305f7f55cc3e
Jyryky <kratkyvlas@email.cz>
2020-01-22 13:30:37 +0100

    ten košík pořád nefunguje..

--- soubory/cookies.js
+++ soubory/cookies.js
@@ -15,17 +15,21 @@ console.log(x);
 return (cname)
 }
 
-function getCookie() {
-    var getting = browser.cookies.getAll(
-        details                // object
-      )
-    for (i = 0; i < 100; i++){
-        var v = document.cookie.test('(^|;) ?' + i + '=([^;]*)(;|$)');
-        if (v==true){
-            console.log(i);
-            return(i);
-        }
-    }
-    
+
+function get_cookies_array() {
+
+  var cookies = { };
+
+  if (document.cookie && document.cookie != '') {
+      var split = document.cookie.split(';');
+      for (var i = 0; i < split.length; i++) {
+          var name_value = split[i].split("=");
+          name_value[0] = name_value[0].replace(/^ /, '');
+          cookies[decodeURIComponent(name_value[0])] = decodeURIComponent(name_value[1]);
+      }
+  }
+  console.log(cookies);
+  return cookies;
+ 
 }
 
--- soubory/zobrazitkosik.php
+++ soubory/zobrazitkosik.php
@@ -6,6 +6,7 @@
 <!-- <script src="jquery-3.4.1.min.js"></script> -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="cookies.js"></script>
+<script src="ajax_nacteni_kosiku.js"></script>
 <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
 <link type="text/css" rel="stylesheet" href="css/style.css"/>
 
@@ -14,9 +15,10 @@
 <style>
 html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
 </style>
-<body>
-<script>
-onload.getCookie();   
-</script>
+<body onload="get_cookies_array()">
+<form method="post" action="">
+<button value='cookies' type="submit">audio</button>
+</form>
+
 </body>
 </html>
