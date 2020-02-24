c95be56308b4012058a60083b70695e0381e6930
Jyryky <kratkyvlas@email.cz>
2020-02-24 21:08:26 +0100

    je nová  databáze je potřeba všechno upravit

--- soubory/index.php
+++ soubory/index.php
@@ -37,7 +37,6 @@ if(isset($_POST["add_to_cart"]))
 		$_SESSION["kosik"][0] = $item_array;
 	}
 }
-
 if(isset($_GET["action"]))
 {
 	if($_GET["action"] == "delete")
@@ -100,50 +99,7 @@ if(isset($_GET["action"]))
 					}
 				}
 			?>
-			<div style="clear:both"></div>
-			<br />
-			<h3>Order Details</h3>
-			<div class="table-responsive">
-				<table class="table table-bordered">
-					<tr>
-						<th width="40%">Item Name</th>
-						<th width="10%">Quantity</th>
-						<th width="20%">Price</th>
-						<th width="15%">Total</th>
-						<th width="5%">Action</th>
-					</tr>
-					<?php
-					if(!empty($_SESSION["kosik"]))
-					{
-						$total = 0;
-						foreach($_SESSION["kosik"] as $keys => $values)
-						{
-					?>
-					<tr>
-						<td><?php echo $values["item_name"]; ?></td>
-						<td><?php echo $values["item_quantity"]; ?></td>
-						<td>$ <?php echo $values["item_price"]; ?></td>
-						<td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2);?></td>
-						<td><a href="index.php?action=delete&ID=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>
-					</tr>
-					<?php
-							$total = $total + ($values["item_quantity"] * $values["item_price"]);
-						}
-					?>
-					<tr>
-						<td colspan="3" align="right">Total</td>
-						<td align="right">$ <?php echo number_format($total, 2); ?></td>
-						<td></td>
-					</tr>
-					<?php
-					}
-					?>
-						
-				</table>
-			</div>
-		</div>
-	</div>
-	<br />
+			
 	</body>
 </html>
 
--- soubory/zobrazitkosik.php
+++ soubory/zobrazitkosik.php
@@ -9,15 +9,14 @@
 <script src="ajax_nacteni_kosiku.js"></script>
 <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
 <link type="text/css" rel="stylesheet" href="css/style.css"/>
-
-
-<script src="vyber-produktu.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <style>
 html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
 </style>
 <body onload="get_cookies_array()">
 <form method="post" action="">
-<button value='cookies' type="submit">audio</button>
+<a href=index.php>Zpět na hlavní stránku</a>
 <?php
 /*$db_user="root";
 $db_pass="";
@@ -37,7 +36,8 @@ echo $pocet_cookies;*/
 
 
 
-//if(isset($_SESSION["kosik"])){ ?>
+//if(isset($_SESSION["kosik"])){
+    session_start(); ?>
     <div style="clear:both"></div>
     <br />
     <h3>Order Details</h3>
